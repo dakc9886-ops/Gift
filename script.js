61e78ffbc09db6ec1757e2ed25d3b2a7667ab747
dakc9886-ops <dakc9886@gmail.com>
2026-01-19 10:18:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const audio = document.getElementById("bgm");
+audio.muted = false;
+audio.play();
+
+setInterval(() => {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.innerHTML = "❤️";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = Math.random() * 30 + 10 + "px";
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}, 300);
